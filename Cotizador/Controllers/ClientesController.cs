bde017fd722b0a7dab5938fd9760b5d0ce1887f4
Ruben alexander Batista santos <alexbatista158@hotmail.com>
2018-05-03 17:25:11 -0400

    View De Cotizacion Terminada, y View de  Clientes Creada.

---
+++ Cotizador/Controllers/ClientesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cotizador.Controllers
+{
+    public class ClientesController : Controller
+    {
+        Models.CotizadorContext context = new Models.CotizadorContext();
+
+        //Clientes Que Contiene El Sistema.
+        public ActionResult Lista()
+        {
+            var lista = context.cliente.ToList();
+            return View(lista);
+        }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(Models.Clientes Cliente)
+        {
+            context.cliente.Add(Cliente);
+            context.SaveChanges();
+
+            return RedirectToAction("Lista");
+        }
+
+    }
+}
