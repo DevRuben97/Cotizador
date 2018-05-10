2ac3c88b7129a73909fc62590d52e2e7c153309a
Ruben alexander Batista santos <alexbatista158@hotmail.com>
2018-05-10 10:29:24 -0400

    Trabajo Con Tabla Clientes.
    Terminacion de los views de cotizador, agregando funciones Crud con ajax.

--- Cotizador/Controllers/ClientesController.cs
+++ Cotizador/Controllers/ClientesController.cs
@@ -37,6 +37,49 @@ namespace Cotizador.Controllers
 
            
         }
+        [HttpGet]
+        public ActionResult Eliminar(int? id)//Confirmar la Eliminacacion
+        {
+            if (id== null)
+            {
+                ViewBag.Error = "Tienes Que Pasar Un ID";
+            }
+            var cliente = context.cliente.Find(id);
+
+            if (cliente == null)
+            {
+                ViewBag.Error = "No Se Pudo Encontrar El Cliente Solicitado";
+            }
+            return PartialView(cliente);
+        }
+        [HttpPost]
+        public PartialViewResult ConfDelete(Models.Clientes cliente)//Eliminar El Cliente seleccionado.
+        {
+
+            context.cliente.Remove(cliente);
+            
+            return PartialView();
+        }
+        [HttpPost]
+        public JsonResult Buscar(string nombre)
+        {
+           var contenido= context.cliente.ToList().Where(x => x.nombre.Equals(nombre)).ToList();
+           
+            return Json("");
+        }
+        public PartialViewResult Detalles(int id )
+        {
+           
+                var cliente = context.cliente.Find(id);
+                if (cliente == null)
+                {
+                    ViewBag.Error = "No Se Pudo Encontrar Los Datos Requeridos";
+                }
+                return PartialView("_PDetalles", cliente);
+            
+           
+           
+        }
 
     }
 }
---
+++ Cotizador/Scripts/MyScripts/Validations.js
@@ -0,0 +1,12 @@
+﻿
+function ValidarClientes() {
+    //Declaracion de las variables:
+    $("#Guardar").click(Valores)
+
+   
+
+}
+function Valores() {
+
+    alert("Hola ComO Estan Todos");
+}
