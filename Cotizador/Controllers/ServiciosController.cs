ac0317e5a9210a4b7ccc9cca8c4a44ef2ddc849a
Ruben alexander Batista santos <alexbatista158@hotmail.com>
2018-05-15 17:15:28 -0400

    View De Servicios, Muestra de Datos Completado.
    Muestra de datos Utilizando Jquery Datatable Completado con obtención de datos en backend.

--- Cotizador/Controllers/ServiciosController.cs
+++ Cotizador/Controllers/ServiciosController.cs
@@ -14,10 +14,100 @@ namespace Cotizador.Controllers
         {
             return View();
         }
-        public JsonResult GetList()
+        [HttpPost]
+        public JsonResult GetServices()
         {
-            var Datos = context.servicio.ToList();
-            return Json(new { data = Datos }, JsonRequestBehavior.AllowGet);
+            JsonResult jsonResult = new JsonResult();
+            try
+            {
+                //Obtener Los Parametros Que Envia Datatable Al Servidor:
+                string Buscador = Request.Form.GetValues("search[value]")[0];
+                string draw = Request.Form.GetValues("draw")[0];
+                string order = Request.Form.GetValues("order[0][column]")[0];
+                string orderDir = Request.Form.GetValues("order[0][dir]")[0];
+                int startRec = Convert.ToInt32(Request.Form.GetValues("start")[0]);
+                int pageSize = Convert.ToInt32(Request.Form.GetValues("length")[0]);
+                //Carga De Datos:
+                var servicos = context.servicio.ToList();
+                //Total De Registros:
+                int totalRecords = servicos.Count();
+
+                //Aplicar El Buscador Si Este Es Requerdido:
+                if (!(string.IsNullOrEmpty(Buscador) || string.IsNullOrEmpty(order)))
+                {
+                   servicos= servicos.Where(x => x.nombre.ToLower().Contains(Buscador)
+                    || x.Descripcion.ToLower().Contains(Buscador)
+                    || x.Costo.Equals(Buscador)).ToList();
+                }
+                //Ordenar los datos en Base A Los Datos:
+                if (!string.IsNullOrEmpty(order) && !string.IsNullOrEmpty(orderDir))
+                {
+                   if (order.Equals("0"))
+                    {
+                        if (orderDir.Equals("asc"))
+                        {
+                            servicos = servicos.OrderBy(x => x.nombre).ToList();
+                        }
+                        else
+                        {
+                            servicos = servicos.OrderByDescending(x => x.nombre).ToList();
+                        }
+                    }
+                   else if (order.Equals("1"))
+                    {
+                        if (orderDir.Equals("asc"))
+                        {
+                            servicos = servicos.OrderBy(x => x.Descripcion).ToList();
+                        }
+                        else 
+                        {
+                            servicos = servicos.OrderByDescending(x => x.Descripcion).ToList();
+                        }
+                    }
+                   else if (order.Equals("2"))
+                    {
+                        if (orderDir.Equals("asc"))
+                        {
+                            servicos = servicos.OrderBy(x => x.Costo).ToList();
+                        }     
+                        else
+                        {
+                            servicos = servicos.OrderByDescending(x => x.Costo).ToList();
+                        }
+                    }
+                }
+
+                int recFilter = servicos.Count();
+                servicos.Skip(startRec).Take(pageSize);
+
+                //Retornar Los Datos:
+                jsonResult = this.Json(new
+                {
+                    draw = Convert.ToInt32(draw),
+                    recordsTotal = totalRecords,
+                    recordsFiltered = recFilter,
+                    data = servicos
+                }, JsonRequestBehavior.AllowGet);
+
+                return jsonResult;
+
+            }
+            catch(Exception ex)
+            {
+                return Json(new { Error = ex.Message });
+            }
         }
+        public ActionResult Nuevo()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo()
+        {
+
+            return View();
+        }
+
     }
 }
